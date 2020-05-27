334f3ccef74301ab45d851aeb1ca80ed92941b8e
Milos Nikolic <misha83@gmail.com>
2020-05-27 20:50:00 +0200

    nnn

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
